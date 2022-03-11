e02332491b9a1246c3b79925d9323aeb40dde035
tashapiro <shapiro.tan@gmail.com>
2022-03-11 11:49:36 -0500

    updating repo

--- shiny-app/server.R
+++ shiny-app/server.R
@@ -1,14 +1,19 @@
 server <- function(input, output, session) {
   
   #load data
-  audio <- read.csv('../data/audio_features.csv')
-  artists<-read.csv('../data/artists2.csv')
-  words<-read.csv('../data/lyrics_words.csv')
-  bis<-read.csv('../data/lyrics_bis.csv')
+  audio <- read.csv('https://raw.githubusercontent.com/tashapiro/predicting-song-music-genre/main/data/audio_features.csv')
+  artists<-read.csv('https://raw.githubusercontent.com/tashapiro/predicting-song-music-genre/main/data/artists2')
+  words<-read.csv('https://raw.githubusercontent.com/tashapiro/predicting-song-music-genre/main/data/lyrics_words.csv')
+  bis<-read.csv('https://raw.githubusercontent.com/tashapiro/predicting-song-music-genre/main/data/lyrics_bis.csv')
   
   #base links
   base = 'https://drive.google.com/uc?id='
   spotify_base = 'https://open.spotify.com/track/'
+  
+  #palettes
+  
+  bcolor <- "#000000"
+  fcolor <- "#FFFFFF"
   
 #TRANSFORM DATA
 songs<-audio%>%
@@ -28,7 +33,11 @@ songs<-audio%>%
            danceability >= input$danceability[1]  &
            danceability <= input$danceability[2] & 
            acousticness >= input$acousticness[1]  &
-           acousticness <= input$acousticness[2] 
+           acousticness <= input$acousticness[2] &
+           instrumentalness >= input$instrumentalness[1]  &
+           instrumentalness <= input$instrumentalness[2]  &
+           speechiness >= input$speechiness[1]  &
+           speechiness <= input$speechiness[2] 
     )%>%
     arrange(-valence) })
 
@@ -66,13 +75,14 @@ songs<-audio%>%
             searchable=TRUE,
             theme = reactableTheme(
               cellStyle=list(display='flex',flexDirection='column',justifyContent='center'),
-              style=list(fontFamily='Roboto')
+              style=list(fontFamily='Roboto'), backgroundColor=bcolor, color=fcolor,
+              borderColor = "#303030"
             ),
             columns= list(
               image_url = colDef(name="",cell=embed_img(height=50,width=50)),
               artist = colDef(name='ARTIST'),
               track = colDef(name='TRACK', html=TRUE, cell= function(value,index){
-                sprintf('<a style=text-decoration:none;color:#3E6F9A; href="%s" target="_blank">%s</a>', df()$song_url[index], value)
+                sprintf('<a style=text-decoration:none;color:#3DCEEE; href="%s" target="_blank">%s</a>', df()$song_url[index], value)
               }),
               genre = colDef(name='GENRE', align='center'),
               valence = colDef(name="VALENCE",style = color_scales(df()), align="center", format=colFormat(digits=2)),
@@ -90,11 +100,19 @@ songs<-audio%>%
                      text = paste0("Artist: ", artist,"\nTrack: ",track,
                                    '\n',input$xScatter,': ',get(input$xScatter),
                                    '\n',input$yScatter,': ', get(input$yScatter)),
-                     fill=genre), alpha = 0.75, stroke=0.25, size=3, shape=21, color='white')+
+                     fill=genre), alpha = 0.9, stroke=0.2, size=3, shape=21, color='white')+
       scale_fill_manual(values=col_pal)+
       facet_wrap(~genre)+
       labs(y=toupper(input$yScatter), x=toupper(input$xScatter))+
-      theme_minimal()
+      theme_minimal()+
+      theme(
+        plot.background = element_rect(fill=bcolor),
+        panel.background = element_rect(fill=bcolor),
+        text = element_text(color=fcolor),
+        strip.text= element_text(color=fcolor),
+        axis.text = element_text(color=fcolor),
+        panel.grid = element_line(color="grey30")
+      )
     
     ggplotly(p, height=600, width=900, tooltip='text')%>%
       layout(font=list(family="Roboto"),
@@ -105,9 +123,10 @@ songs<-audio%>%
   output$wordCloud<- renderPlot({
     set.seed(42)
     ggplot(df_words(), aes(label = word, size = word_count)) +
-      geom_text_wordcloud_area(eccentricity = 1) +
+      geom_text_wordcloud_area(eccentricity = 1, color=fcolor) +
       scale_size_area(max_size = 24) +
-      theme_minimal()
+      theme_minimal()+
+      theme(panel.background = element_rect(fill=bcolor, color=bcolor))
   })
   
   
@@ -125,7 +144,18 @@ songs<-audio%>%
       facet_wrap(~genre, scales='free_y')+
       labs(y="WORDS",x=input$wordMetric)+
       theme_minimal()+
-      theme(legend.position="none")
+      theme(
+        legend.position="none",
+        plot.background = element_rect(fill=bcolor),
+        panel.background = element_rect(fill=bcolor),
+        text = element_text(color=fcolor),
+        strip.text=element_text(color=fcolor),
+        axis.text = element_text(color=fcolor),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x= element_line(color="grey50"),
+        panel.grid.minor.x=element_blank()
+      )
     
     ggplotly(p, height=600, width=900, tooltip='text')%>%
       layout(font=list(family="Roboto"),
--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinyWidgets)
+library(shinydashboard)
 library(reactable)
 library(reactablefmtr)
 library(tidyverse)
@@ -7,29 +8,24 @@ library(plotly)
 library(ggridges)
 library(reshape2)
 library(ggwordcloud)
+library(tidytext)
+library(bslib)
 
 ui <- fluidPage(
-
- # sliderInput("valence", label="Select Valence")
-  titlePanel("SONG PROFILES"),
-  tags$head(
-    # Note the wrapping of the string in HTML()
-    tags$style(HTML("
-      body {
-        font-family: 'Roboto', sans-serif;
-      }
-      h3 {
-        font-family: 'Roboto', sans-serif;
-      }              
-                    "))
+  
+  theme = bs_theme(
+    bg = "#000000", fg = "white", primary = '#FFBC42', #"#FFCF00",
+    base_font = font_google("Roboto"),
+    code_font = font_google("Roboto")
   ),
+
+  h2("MUSIC OR LYRICS: Song Genre Classification"),
+  h3(""),
   sidebarLayout(
     sidebarPanel(width=3,
-      h3("ABOUT"),
-      "Data collected using the Spotify API. Clicking on track titles will redirect to Spotify.",
-      h3("FILTERS"),
-      "Change the inputs below to change results displayed in the table.",
-      h3(""),
+      h5("FILTERS"),
+      "Change the inputs below to change results displayed in the main panel.",
+      h5(),
       selectInput(
         inputId="genre",
         label="Music Genre",
@@ -37,6 +33,8 @@ ui <- fluidPage(
         selected=c("Country","Dance Pop","Hip Hop","Rock"),
         multiple=TRUE
       ),
+      h5(""),
+      conditionalPanel(condition="input.tabset==3 | input.tabset==4",
       sliderInput(
         "valence",
         label="Valence",
@@ -61,88 +59,89 @@ ui <- fluidPage(
         min=0, max=1,
         value=c(0,1)
       ),
+      sliderInput(
+        "instrumentalness",
+        label="Instrumentalness",
+        min=0, max=1,
+        value=c(0,1)
+      ),
+      sliderInput(
+        "speechiness",
+        label="Speechiness",
+        min=0, max=1,
+        value=c(0,1)
+      )
+    ),
+    conditionalPanel(condition="input.tabset==2",
+                              sliderInput("nWords",
+                                          label="Number of Top Words:",
+                                          min=20, max=150,
+                                          value=100),
+                              radioButtons("includeStopWords", "Stop Words:",
+                                           inline=TRUE,
+                                           c("Include" = "yes",
+                                             "Exclude" = "no")
+                              )
+                     ),
+    conditionalPanel(condition="input.tabset==1",
+       radioButtons("includeStop", "Stop Words:",
+                    inline=TRUE,
+                    c("Include" = "yes",
+                      "Exclude" = "no"),
+                    selected="no"
+       ),
+       selectInput("nGrams",
+                   label="Gram Split:",
+                   choices=c("One"=1,
+                             "Two"=2),
+                   selected=1),
+       selectInput("wordMetric",
+                   label="Word Metric:",
+                   choices=c("Word Count"="word_count",
+                             "Songs Containg"="songs",
+                             "Percentage of Songs" = "song_perc"),
+                   selected="word_count"),
+       selectInput("wordType",
+                   label="Word Type",
+                   choices=c("All" = "all",
+                             "Action Verbs" = "action_verb",
+                             "Irregular Nouns" = "irr_noun",
+                             "Pronouns" = "pronoun",
+                             "Drugs & Alcohol" = "subst",
+                             "Racist Words" = "racist"
+                   )),
+       sliderInput("nWordsTop",
+                   label="Number of Grams:",
+                   min=5,max=15,
+                   value=10)
+    )
     ),
     mainPanel(
       tabsetPanel(
-        id = "tabset",
-        tabPanel("Top Grams",
-                 h4("Parameters"),
-                 fluidRow(
-                 column(4,
-                 selectInput("nGrams",
-                             label="Gram Split:",
-                             choices=c("One"=1,
-                                       "Two"=2),
-                             selected=1),
-                 selectInput("wordMetric",
-                             label="Word Metric:",
-                             choices=c("Word Count"="word_count",
-                                       "Songs Containg"="songs",
-                                       "Percentage of Songs" = "song_perc"),
-                             selected="word_count")
-                 ), 
-                 column(4,
-                  selectInput("wordType",
-                              label="Word Type",
-                              choices=c("All" = "all",
-                                        "Action Verbs" = "action_verb",
-                                        "Irregular Nouns" = "irr_noun",
-                                        "Pronouns" = "pronoun",
-                                        "Drugs & Alcohol" = "subst",
-                                        "Racist Words" = "racist"
-                              )),
-                  radioButtons("includeStop", "Stop Words:",
-                               inline=TRUE,
-                               c("Include" = "yes",
-                                 "Exclude" = "no"),
-                               selected="no"
-                  )
-                 ),
-                 column(4, 
-                        sliderInput("nWordsTop",
-                                    label="Number of Grams:",
-                                    min=5,max=15,
-                                    value=10)
-                 )
-                 ),
+        id = "tabset", 
+        tabPanel("Top Grams",value=1,
                  plotlyOutput("topWords")
                  ),
-        tabPanel("Word Cloud",
-                 h4("Parameters"),
-                 fluidRow(
-                     column(5,
-                     sliderInput("nWords",
-                                 label="Number of Top Words:",
-                                 min=20, max=150,
-                                 value=100)
-                     ),
-                     column(5,
-                     radioButtons("includeStopWords", "Stop Words:",
-                                  inline=TRUE,
-                                  c("Include" = "yes",
-                                    "Exclude" = "no")
-                                  )
-                     )
-                 ),
+        tabPanel("Word Cloud", value=2,
                  plotOutput("wordCloud", height="500px")
       ),
-        tabPanel("Cross Analysis",
-           h4("Parameters"),
+        tabPanel("Cross Analysis", value=3,
+           h5("Variables"),
            fluidRow(
             column(5,
             selectInput("xScatter", 
                         label="X Variable",
-                        choices=c("valence","danceability","energy","acousticness"),
+                        choices=c("acousticness","danceability","energy","instrumentalness","speechiness","valence"),
                         selected="valence")
             ),
             column(5,
             selectInput("yScatter", 
                         label="Y Variable",
-                        choices=c("valence","danceability","energy","acousticness"),
+                        choices=c("acousticness","danceability","energy","instrumentalness","speechiness","valence"),
                         selected="danceability")
            )),
             plotlyOutput("plotlyScatter")),
-        tabPanel("Audio Profile",
+        tabPanel("Audio Profile", value=4,
                  reactableOutput("songTable")
         )
     )
